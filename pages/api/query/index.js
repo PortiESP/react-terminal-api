ba07ab65479769531959009b5dda6747f15b70b0
Porti <elp0rti201@gmail.com>
2022-08-11 17:23:23 +0200

    added logging

--- pages/api/query/index.js
+++ pages/api/query/index.js
@@ -1,6 +1,11 @@
 const exec = require("child_process").exec
 
 export default function handler(req, res) {
+
+    console.log()
     exec("cd testenv;" + req.query.cmd, (stdin, stdout, stderr)=> res.status(200).json({ query: req.query.cmd, stdout, stdin, stderr}))   
+    
+    // Log
+    exec(`echo "[${req.connection.remoteAddress}] - [${req.url}] - [${req.rawHeaders[req.rawHeaders.findIndex( h => h === "User-Agent")+1]}]" >> logs.txt`)
 }
   
