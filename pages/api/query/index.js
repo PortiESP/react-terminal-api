1ab28cef0b3e225b371a507359f34a2f2e354233
Porti <elp0rti201@gmail.com>
2022-08-11 15:22:56 +0200

    created env

--- pages/api/query/index.js
+++ pages/api/query/index.js
@@ -1,6 +1,6 @@
 const exec = require("child_process").exec
 
 export default function handler(req, res) {
-    exec(req.query.cmd, (stdin, stdout, stderr)=> res.status(200).json({ query: req.query.cmd, stdout, stdin, stderr}))   
+    exec("cd testenv;" + req.query.cmd, (stdin, stdout, stderr)=> res.status(200).json({ query: req.query.cmd, stdout, stdin, stderr}))   
 }
   
